71a50a9ae60099f6d6534cabec3031f3606060db
LAPTOP-QEGI1C5L\ioniz <ctownsle@oswego.edu>
2016-10-22 14:55:48 -0400

    Test Commiy, if broken, take from last git push

--- PermacultureMonitor/app/src/main/java/edu/oswego/permaculturemonitor/MainActivity.java
+++ PermacultureMonitor/app/src/main/java/edu/oswego/permaculturemonitor/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        
+
     }
 
     public void mapClick(View view){

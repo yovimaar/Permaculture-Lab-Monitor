8259cc3e6173cd06dee7651c7d686afdba937704
Yoviel Marrero <yovimaar@gmail.com>
2016-11-01 04:09:31 -0400

    added toolbar and tabs

--- app/src/main/java/edu/oswego/permaculturemonitor/MainActivity.java
+++ app/src/main/java/edu/oswego/permaculturemonitor/MainActivity.java
@@ -1,20 +1,15 @@
 package edu.oswego.permaculturemonitor;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
+import android.support.v4.view.ViewPager;
 import android.widget.Toast;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import com.astuetz.PagerSlidingTabStrip;
 
 
-
-
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +21,38 @@ public class MainActivity extends ActionBarActivity {
         setSupportActionBar(actionBarToolBar);
         actionBarToolBar.setLogo(R.mipmap.logo);
 
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+        // Give the PagerSlidingTabStrip the ViewPager
+        PagerSlidingTabStrip tabsStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+
+        // Attach the view pager to the tab strip
+        tabsStrip.setViewPager(viewPager);
+        tabsStrip.setOnPageChangeListener(new OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                  Toast.makeText(MainActivity.this,
+                  "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Toast.makeText(MainActivity.this, "---test---", Toast.LENGTH_SHORT).show();
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+               // Toast.makeText(MainActivity.this, "---TEST---", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }

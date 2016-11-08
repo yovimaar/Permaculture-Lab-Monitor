04b23fec0c5b9d119ad941da3be3aceff87d91ac
yov <yovimaar@gmail.com>
2016-11-08 01:10:09 -0500

    fix build problem

---
+++ app/src/main/java/edu/oswego/permaculturemonitor/ListView.java
@@ -0,0 +1,59 @@
+package edu.oswego.permaculturemonitor;
+
+import android.app.ActionBar;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+
+
+/**
+ * Created by ioniz on 11/2/2016.
+ */
+
+public class ListView extends ListActivity  {
+
+
+    // These are the elements for the list
+    String [] listItems = {"pH", "Soil Moisture", "Temperature", "Humidity"};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view);
+
+        // Create a progress bar to display while the list loads
+        ProgressBar progressBar = new ProgressBar(this);
+        progressBar.setLayoutParams(new ActionBar.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
+        progressBar.setIndeterminate(true);
+        getListView().setEmptyView(progressBar);
+
+        // Must add the progress bar to the root of the layout
+        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+        root.addView(progressBar);
+
+        // Create an adapter we will use to display data.
+       ArrayAdapter<String> mAdapter = new ArrayAdapter<>(getListView().getContext(),
+                android.R.layout.simple_list_item_1, listItems);
+        getListView().setAdapter(mAdapter);
+
+
+
+    }
+
+
+
+
+//fix this when we get everything else set.
+   // @Override
+    //public void onListItemClick(ListView l, View v, int position, long id) {
+        // Do something when a list item is clicked
+    //}
+}
+
